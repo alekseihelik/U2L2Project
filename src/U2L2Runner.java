80f686c98f9bd43bdc8e854be49ba7ecaa95c04d
alekseihelik <ahelik4984@bths.edu>
2022-09-27 20:59:26 -0400

    added ConstructionPricer

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,20 +1,15 @@
+import java.util.Scanner;
 public class U2L2Runner {
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(150,200);
-        Rectangle rect2 = new Rectangle(100);
-        Rectangle rect3 = new Rectangle();
-        rect1.setWidth(125);
-        rect2.setWidth(125);
-        rect2.setLength(125);
-        rect3.setWidth(125);
-        int rect1Perimeter = rect1.calculatePerimeter();
-        int rect2Perimeter = rect2.calculatePerimeter();
-        int rect3Perimeter = rect3.calculatePerimeter();
-        int totalFencing = rect1Perimeter + rect2Perimeter + rect3Perimeter;
-        int rect1Area = rect1.calculateArea();
-        int rect2Area = rect2.calculateArea();
-        int rect3Area = rect3.calculateArea();
-        int totalArea = rect1Area + rect2Area + rect3Area;
-        System.out.println("The total fencing needed to enclose all three plots of land will be " + totalFencing + " feet and the total area that needs seeding is " + totalArea + " square feet.");
+        Scanner s = new Scanner(System.in);
+        System.out.print("How many pieces of lumber do you need for the home you are building?");
+        int lumber = s.nextInt();
+        System.out.print("How many windows do you need?");
+        int windows = s.nextInt();
+        System.out.print("What is the tax rate in your area?");
+        double taxRate = s.nextDouble();
+        ConstructionPricer prices = new ConstructionPricer(11.50, 25.75, taxRate);
+        System.out.println("Total Materials Cost: $" + prices.materialsCost(lumber, windows));
+        System.out.println("Grand Total after Tax: $" + prices.totalWithTax(prices.materialsCost(lumber, windows)));
     }
 }
